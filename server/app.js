5f1c4b3f486acfa4c46886f01d1405c1563b3ad6
CallumCheng <callumcheng@outlook.com>
2022-07-18 14:58:15 +0100

    created server, started server and menu items

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const e = require("express");
+const express = require("express");
+
+const app = express();
+
+const menu = {
+    starters: ["garlic bread", "chocolate fudge cake", "fries"],
+    main: ["burger and chips", "steak and mash potatoes", "chicken katsu curry"],
+    desserts: ["pancakes and syrup", "ice cream", "creme brulee"],
+    specials: {
+        "monday": "dover sole",
+        "tuesday": "porterhouse steak",
+
+    }
+}
+
+app.get("/", (req, res) => {
+    res.json({
+        "name": "Lucky Oriental",
+        "established": 1975,
+        "status-rating": 10,
+        "hygiene-rating": 10
+    })
+})
+
+app.get("/menu", (req, res) => {
+    res.json(menu)
+})
+
+// /specials/monday... tuesday... etc.
+app.get("/specials/:day", (req,res) => {
+    const day = req.params.day;
+    const special = menu["specials"][day];
+
+    if (special) {
+        res.json({
+            "special": special
+        });
+    } else {
+        res.status(404).json({       //setting page not found if not correct terms put into addresss
+            "special": null,
+            "success": false
+        })
+    }
+})
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`You are now listening in on port ${port}. Sit back, relax, and enjoy the show.`)
+})
